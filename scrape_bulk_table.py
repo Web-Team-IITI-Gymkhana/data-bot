6808d9e69ea7d0afd07ee3a0cf6f421669d47999
Mihir Karandikar <mihirkkarandikar@gmail.com>
2022-03-11 10:20:25 +0530

    Fix table scraping logic

--- scrape_bulk_table.py
+++ scrape_bulk_table.py
@@ -10,14 +10,16 @@ for cik in ciks:
     try:
         df = scrape_table.get_sheet(cik,"10-K","20200101", "20210101")
         df.to_csv("./temp/{}.csv".format(str(cik)+" 10-K"),index=False)
-    except:
+    except Exception as e:
+        print("Exception in scrape_bulk 10k ",e)
         continue
 
 for cik in ciks:
     try:
         df = scrape_table.get_sheet(cik,"10-Q","20200101", "20210101")
         df.to_csv("./results/{}.csv".format(str(cik) + " 10-Q"),index=False)
-    except:
+    except Exception as e:
+        print("Exception in scrape_bulk 10q ",e)
         continue
 
 print("Done with bulk scraping")
