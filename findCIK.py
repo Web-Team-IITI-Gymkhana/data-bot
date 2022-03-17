cc29b65cc476bdc5e70ebf1521fd63e058f40e61
Mihir Karandikar <mihirkkarandikar@gmail.com>
2022-03-17 17:13:13 +0530

    Add comments

--- findCIK.py
+++ findCIK.py
@@ -1,63 +1,33 @@
 import pandas as pd
 import re
-
-def withRepeat():
-    companyNames = pd.read_csv('Software Company List v2.csv')
-    names = companyNames['Company'].tolist()
-    data = {'CompanyName':[],'CIK':[]}
-    data_file = pd.DataFrame(data)
-    for name in names:
-        pattern = name
-        pattern = pattern.strip()
-        fhand = open('cik-lookup-data.txt')
-        for line in fhand:
-            target = line.strip()
-
-            target = target.replace(',','')
-
-            hit = re.findall('^{}'.format(pattern),target)
-            
-            if len(hit) == 0:
-                continue
-            else:
-                cik = re.findall(':(000.*):',target)
-                details = {'CompanyName': pattern , 'CIK': cik[0]}
-                data_file = data_file.append(details, ignore_index = True)         
-        
-        fhand.close()
-
-
-    data_file.to_csv('Result_Repeat.csv',index=False)
-            
-    print("Done Repeat")     
-
    
-def noRepeat():
+def findCik():
     companyCIK = dict()
     companyNames = pd.read_csv('Software Company List v2.csv')
     names = companyNames['Company'].tolist()
     for name in names:
         pattern = name
         pattern = pattern.strip()
-        fhand = open('cik-lookup-data.txt')
+        fhand = open('cik-lookup-data.txt') #We scrape the cik-lookup-data.txt document in order to obtain 
+                                            #the cik ids corressponding to the company names provided in the list
         for line in fhand:
             target = line.strip()
 
-            target = target.replace(',','')
+            target = target.replace(',','') 
 
-            hit = re.findall('^{}'.format(pattern),target)
+            hit = re.findall('^{}'.format(pattern),target) #Check if company name exists in the line
             
             if len(hit) == 0:
                 continue
             else:
-                cik = re.findall(':(000.*):',target)
+                cik = re.findall(':(000.*):',target) #If company name found, we extract the cik id using this regular expression
                 companyCIK[pattern] = companyCIK.get(pattern,cik[0])
         fhand.close()
     
     result = pd.DataFrame(list(companyCIK.items()), columns=['CompanyName', 'CIK'])
     result.to_csv('Result_No_Repeat.csv',index=False)
             
-    print("Done No Repeat")  
+    print("Done")
 
 
-noRepeat()
+findCik()
